294b22de32c324a85bc8ea02c7a4f6c09d02c01f
ajayidhikrullah <ajayidhikrullah@gmail.com>
2019-09-21 18:57:18 +0100

    created the footer and header blde templated for the frontend

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,53 @@
+    <div class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3">
+            <p class="mb-4"><img src={{asset("assets/images/hajjquote3.jpg")}} alt="Image" class="img-fluid"></p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nemo minima qui dolor, iusto iure.</p>  
+            <p><a href="#">Learn More</a></p>
+          </div>
+          <div class="col-lg-3">
+            <h3 class="footer-heading"><span>Our Company</span></h3>
+            <ul class="list-unstyled">
+                <li><a href="#">About</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Our Team</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Projects</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-3">
+              <h3 class="footer-heading"><span>Our Services</span></h3>
+              <ul class="list-unstyled">
+                  <li><a href="#">Hajj</a></li>
+                  <li><a href="#">Umrah</a></li>
+                  <li><a href="#">Ramadan PAckages</a></li>
+              </ul>
+          </div>
+          <div class="col-lg-3">
+              <h3 class="footer-heading"><span>Contact</span></h3>
+              <ul class="list-unstyled">
+                  <li><a href="#">Help Center</a></li>
+                  <li><a href="#">Support Community</a></li>
+                  <li><a href="#">Press</a></li>
+                  <li><a href="#">FAQ</a></li>
+                  <li><a href="#">Our Partners</a></li>
+              </ul>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-12">
+            <div class="copyright">
+                <p>
+                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://www.github.com/ajayidhikrullah" target="_blank" >Dhikrullah</a> 
+                    </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+
+  </div>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,63 @@
+<div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+  
+  
+      
+      <div class="header-top bg-light">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-6 col-lg-3">
+              <a href="index.html">
+                <h1>Allahu Muhammad Central Mosque</h1>
+                {{-- <img src="images/logo.png" alt="Image" class="img-fluid"> --}}
+              </a>
+            </div>
+            <div class="col-lg-3 d-none d-lg-block">
+  
+              <div class="quick-contact-icons d-flex">
+                <div class="icon align-self-start">
+                  <span class="flaticon-placeholder text-primary"></span>
+                </div>
+                <div class="text">
+                  <span class="h4 d-block">Lagos, Nigeria</span>
+                  <span class="caption-text">Ikotun, Governors Road, LA</span>
+                </div>
+              </div>
+  
+            </div>
+            <div class="col-lg-3 d-none d-lg-block">
+              <div class="quick-contact-icons d-flex">
+                <div class="icon align-self-start">
+                  <span class="flaticon-call text-primary"></span>
+                </div>
+                <div class="text">
+                  <span class="h4 d-block">081 2811 4951</span>
+                  <span class="caption-text">Toll free</span>
+                </div>
+              </div>
+            </div>
+  
+            <div class="col-lg-3 d-none d-lg-block">
+              <div class="quick-contact-icons d-flex">
+                <div class="icon align-self-start">
+                  <span class="flaticon-email text-primary"></span>
+                </div>
+                <div class="text">
+                  <span class="h4 d-block">ajayidhikrullah@gmail.com</span>
+                  <span class="caption-text">Anisere Street, Off Governors Road, Ikotun</span>
+                </div>
+              </div>
+            </div>
+  
+            <div class="col-6 d-block d-lg-none text-right">
+                <a href="#" class="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black"><span
+                  class="icon-menu h3"></span></a>
+            </div>
+          </div>
+        </div>
